5982a30ad09cb066066bdd27f4399945ac14cbf0
duoduo0807 <1437807836@qq.com>
2019-10-21 14:59:18 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set("@", resolve("src"))
-      .set("c", resolve("src/components"));
+      .set("_c", resolve("src/components"));
 
   },
   //打包时不生成.map文件
